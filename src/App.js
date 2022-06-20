352c9f8fc390a2a67e163391c246dcc1c5426bcb
nordeeen <ahmadnrdnsah@gmail.com>
2022-06-20 05:11:34 -0700

    dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 
 function App() {
+  const title = "welcom to the new blog";
+  const likes = 50;
+  const link = "https://www.unsplash.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>{likes}</p>
+        <a href={link}>Unsplash Site</a>
       </div>
     </div>
   );
