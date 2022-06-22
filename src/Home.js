14b7d6a397fda1ccaa2b89699929378afcff447d
nordeeen <ahmadnrdnsah@gmail.com>
2022-06-22 00:35:49 -0700

    adding click events

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,23 @@ import React from "react";
 
 // Multiple Components
 const Home = () => {
+  function handleClick() {
+    alert("hello, ninjas");
+  }
+
+  const handleClickAgain = (nama, e) => {
+    console.log(`hello, ${nama}`, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button style={{ marginLeft: "8px" }} onClick={handleClick}>
+        click me
+      </button>
+      <button onClick={(e) => handleClickAgain("Dian", e)}>
+        click me again
+      </button>
     </div>
   );
 };
