5f1e7a4b9b453447774350abbaaa5d3e1cbff21c
nordeeen <ahmadnrdnsah@gmail.com>
2022-06-22 01:08:36 -0700

    using useState

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 // Multiple Components
 const Home = () => {
-  function handleClick() {
-    alert("hello, ninjas");
-  }
+  const [name, setName] = useState("luigi");
+  const [age, setAge] = useState(30);
 
-  const handleClickAgain = (nama, e) => {
-    console.log(`hello, ${nama}`, e.target);
+  const handleBtn = () => {
+    setName("Jhon Doe");
+    setAge(24);
   };
+
+  // function handleClick() {
+  //   alert("hello, ninjas");
+  // }
+
+  // const handleClickAgain = (nama, e) => {
+  //   console.log(`hello, ${nama}`, e.target);
+  // };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button style={{ marginLeft: "8px" }} onClick={handleClick}>
+      <p style={{ padding: "10px" }}>
+        {name} is {age} years old
+      </p>
+      <button onClick={handleBtn}>Please Click</button>
+
+      {/* <button style={{ marginLeft: "8px" }} onClick={handleClick}>
         click me
       </button>
       <button onClick={(e) => handleClickAgain("Dian", e)}>
         click me again
-      </button>
+      </button> */}
     </div>
   );
 };
